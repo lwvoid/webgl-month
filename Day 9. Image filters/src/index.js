644d0608c0bd2e6bd4b5937daab199af4a69a473
wendli <wendli@tencent.com>
2020-08-18 14:16:53 +0800

    feat: Day 9. Image filters

--- Day 9. Image filters/src/index.js
+++ Day 9. Image filters/src/index.js
@@ -41,14 +41,15 @@ async function main() {
   const textureImage = await GLUtils.loadImage(textureUrl)
   const texture = gl.createTexture()
   gl.bindTexture(gl.TEXTURE_2D, texture)
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureImage)
-
+  
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
-
+  
   gl.activeTexture(gl.TEXTURE0)
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureImage)
+  
   gl.uniform1i(uniformLocation.texture, 0)
   gl.uniform2fv(uniformLocation.resolution, [canvas.width, canvas.height])
 
