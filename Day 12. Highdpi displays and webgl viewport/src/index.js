f344e018469f625025b265a502c0bf9b56b57715
wendli <lwvoid@foxmail.com>
2020-08-21 10:40:04 +0800

    feat: Day 12. Highdpi displays and webgl viewport

---
+++ Day 12. Highdpi displays and webgl viewport/src/index.js
@@ -0,0 +1,62 @@
+import vertexShaderSource from './shader/vertex.glsl'
+import fragmentShaderSource from './shader/fragment.glsl'
+import textureUrl from './assets/texture.jpg'
+import otherTextureUrl from './assets/otherTexture.jpg'
+import GLUtils from './utils/index'
+import { GLBuffer } from './utils/GLBuffer'
+
+
+async function main() {
+
+  const { gl } = GLUtils.initContext()
+
+  const vertexShader = GLUtils.initShader(gl, gl.VERTEX_SHADER, vertexShaderSource)
+  const fragmentShader = GLUtils.initShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource)
+  const program = GLUtils.initProgram(gl, vertexShader, fragmentShader)
+  gl.useProgram(program)
+
+  const {
+    attributeLocations,
+    uniformLocations
+  } = GLUtils.getLocation(gl, program, vertexShaderSource, fragmentShaderSource)
+
+
+  const vertexIndexBuffer = new GLBuffer(gl, gl.ELEMENT_ARRAY_BUFFER, new Uint8Array([
+    0, 1, 2, 2, 3, 0,
+    4, 5, 6, 6, 7, 4,
+  ]), gl.STATIC_DRAW)
+
+
+  const texCoordsBuffer = new GLBuffer(gl, gl.ARRAY_BUFFER, new Float32Array([
+    ...GLUtils.getRectangle(0, 0, 1, 1),
+    ...GLUtils.getRectangle(0, 0, 1, 1),
+  ]), gl.STATIC_DRAW)
+  gl.vertexAttribPointer(attributeLocations.texCoord, 2, gl.FLOAT, false, 0, 0)
+
+
+  const texIndexBuffer = new GLBuffer(gl, gl.ARRAY_BUFFER, new Float32Array([
+    ...Array.from({ length: 4 }).fill(0),
+    ...Array.from({ length: 4 }).fill(1)
+  ]), gl.STATIC_DRAW)
+  gl.vertexAttribPointer(attributeLocations.texIndex, 1, gl.FLOAT, false, 0, 0)
+
+
+  const vertexBuffer = new GLBuffer(gl, gl.ARRAY_BUFFER, new Float32Array([
+    ...GLUtils.getRectangle(-1, -1, 1, 1),
+    ...GLUtils.getRectangle(0, 0, 1, 1),
+  ]), gl.STATIC_DRAW)
+  gl.vertexAttribPointer(attributeLocations.position, 2, gl.FLOAT, false, 0, 0)
+
+
+  gl.activeTexture(gl.TEXTURE0)
+  gl.uniform1i(uniformLocations.texture, 0)
+  await GLUtils.initTexture(gl, textureUrl)
+
+  gl.activeTexture(gl.TEXTURE1)
+  gl.uniform1i(uniformLocations.otherTexture, 1)
+  await GLUtils.initTexture(gl, otherTextureUrl)
+
+  gl.drawElements(gl.TRIANGLES, vertexIndexBuffer.data.length, gl.UNSIGNED_BYTE, 0)
+}
+
+main()
---
+++ Day 12. Highdpi displays and webgl viewport/src/utils/index.js
@@ -0,0 +1,150 @@
+
+
+
+export default {
+  initContext(id = 'canvas') {
+    const canvas = document.getElementById(id)
+    const gl = canvas.getContext('webgl')
+
+    const onResize = () => {
+      const width = window.innerWidth * window.devicePixelRatio
+      const height = window.innerHeight * window.devicePixelRatio
+      canvas.width = width
+      canvas.height = height
+      canvas.style.width = `${width}px`
+      canvas.style.height = `${height}px`
+  
+      gl.viewport(0, 0, canvas.width, canvas.height)
+    }
+
+    onResize()
+    window.addEventListener('resize', onResize)
+
+    return { gl, canvas }
+  },
+
+
+  initShader (gl, shaderType, source) {
+    const shader = gl.createShader(shaderType)
+    gl.shaderSource(shader, source)
+    gl.compileShader(shader)
+
+    const log = gl.getShaderInfoLog(shader)
+    if (log) {
+      throw new Error(log)
+    }
+    return shader
+  },
+
+
+  initProgram (gl, vertexShader, fragmentShader) {
+    const program = gl.createProgram()
+    gl.attachShader(program, vertexShader)
+    gl.attachShader(program, fragmentShader)
+    gl.linkProgram(program)
+
+    const log = gl.getProgramInfoLog(program)
+    if (log) {
+      throw new Error(log)
+    }
+
+    return program
+  },
+
+
+  async initTexture(gl, src) {
+    const image = await this.loadImage(src)
+
+    const texture = gl.createTexture()
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image)
+
+    return texture
+  },
+
+
+
+  getRectangle (x, y, width, height) {
+    const vertices = [
+      x, y,
+      x + width, y,
+      x + width, y + height,
+      x, y + height,
+    ]
+    return vertices
+  },
+
+
+  loadImage (src) {
+    return new Promise((resolve, reject) => {
+      const image = new Image()
+
+      image.onload = () => {
+        resolve(image)
+      }
+
+      image.onerror = (err) => {
+        reject(err, image)
+      }
+
+      image.src = src
+    })
+  },
+
+
+  extract(source) {
+    const attributeRegexp = /attribute\s+.*?\s+(.*)?;/g
+    const uniformRegexp = /uniform\s+.*?\s+(.*)?;/g
+  
+    let attributeResult = null
+    const atttributes = []
+    while ((attributeResult = attributeRegexp.exec(source)) != null) {
+      atttributes.push(attributeResult[1])
+    }
+  
+    let uniformResult = null
+    const uniforms = []
+    while ((uniformResult = uniformRegexp.exec(source)) != null) {
+      uniforms.push(uniformResult[1])
+    }
+  
+    return {
+      atttributes,
+      uniforms
+    }
+  },
+
+
+  getLocation(gl, program, vertexShaderSource, fragmentShaderSource) {
+    const vertexShaderInfo = this.extract(vertexShaderSource)
+    const fragmentShaderInfo = this.extract(fragmentShaderSource)
+
+    const attributes = vertexShaderInfo.atttributes
+    const uniforms = [
+      ...vertexShaderInfo.uniforms,
+      ...fragmentShaderInfo.uniforms,
+    ]
+
+    const attributeLocations = attributes.reduce((attrMap, attrName) => {
+      attrMap[attrName] = gl.getAttribLocation(program, attrName)
+      gl.enableVertexAttribArray(attrMap[attrName])
+      return attrMap
+    }, {})
+
+    const uniformLocations = uniforms.reduce((uniformMap, uniformName) => {
+      uniformMap[uniformName] = gl.getUniformLocation(program, uniformName)
+      return uniformMap
+    }, {})
+
+    return {
+      attributeLocations,
+      uniformLocations,
+    }
+  }
+}
